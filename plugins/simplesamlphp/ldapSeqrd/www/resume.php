0339f55eebeb104ba6ed5cebaf166b1f35768e07
Joe Pletcher <joepletcher@gmail.com>
2013-12-26 17:32:40 -0800

    Initial commit of ldapSeqrd module

---
+++ plugins/simplesamlphp/ldapSeqrd/www/resume.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * simplesamlphp-modules/seqrdauth/www/resume.php
+ *
+ * @package default
+ */
+
+
+/**
+ * This page serves as the point where the user's authentication
+ * process is resumed after the login page.
+ *
+ * It simply passes control back to the class.
+ *
+ * @package simpleSAMLphp
+ * @version $Id$
+ */
+sspmod_ldapSeqrd_Auth_Source_LDAPSeqrd::resume();
